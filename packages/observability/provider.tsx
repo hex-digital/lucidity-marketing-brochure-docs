b96d2cb93c746d67b061bbab2aa14f9a87b06e82
Jamie Warburton <jamie@hexdigital.com>
2026-03-28 16:34:55 +0000

    feat: turn off CWV in BetterStack

--- packages/observability/provider.tsx
+++ packages/observability/provider.tsx
@@ -1,17 +1,5 @@
-import { env } from './env';
 import { SpeedInsights } from '@vercel/speed-insights/next';
-import { BetterStackWebVitals } from '@logtail/next/webVitals';
-
-const { NEXT_PUBLIC_BETTER_STACK_SOURCE_TOKEN, NEXT_PUBLIC_BETTER_STACK_INGESTING_URL } =
-  env();
 
 export function PerformanceMonitorProvider() {
-  return (
-    <>
-      <SpeedInsights />
-      {!!NEXT_PUBLIC_BETTER_STACK_SOURCE_TOKEN && !!NEXT_PUBLIC_BETTER_STACK_INGESTING_URL && (
-        <BetterStackWebVitals />
-      )}
-    </>
-  );
+  return <SpeedInsights />;
 }
