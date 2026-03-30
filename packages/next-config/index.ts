3282aa401780b6d838cfdec4e3d9087c11f63163
Jamie Warburton <jamie@hexdigital.com>
2026-03-30 14:34:27 +0100

    feat: turn off next image optimisation

--- packages/next-config/index.ts
+++ packages/next-config/index.ts
@@ -1,7 +1,11 @@
 import withBundleAnalyzer from '@next/bundle-analyzer';
 import type { NextConfig } from 'next';
 
-export const config: NextConfig = {};
+export const config: NextConfig = {
+  images: {
+    unoptimized: true,
+  },
+};
 
 export const withAnalyzer = (sourceConfig: NextConfig): NextConfig =>
   withBundleAnalyzer()(sourceConfig);
