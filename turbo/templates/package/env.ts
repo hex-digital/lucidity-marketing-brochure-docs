3232b4d79c2a93408beecd7bddc598a09055fdb4
Jamie Warburton <jamie@hexdigital.com>
2026-04-06 12:48:10 +0100

    fix: fix turbo env.ts file template

--- turbo/templates/package/env.ts
+++ turbo/templates/package/env.ts
@@ -3,17 +3,7 @@ import { z } from 'zod';
 
 export const env = () =>
   createEnv({
-    server: {
-      SLACK_BOT_TOKEN: z.string().min(1).optional(),
-      SLACK_FEEDBACK_API_BASE_URL: z.url().optional(),
-    },
-    client: {
-      NEXT_PUBLIC_DOCS_FEEDBACK_SLACK_CHANNEL_ID: z.string().min(1).optional(),
-    },
-    runtimeEnv: {
-      NEXT_PUBLIC_DOCS_FEEDBACK_SLACK_CHANNEL_ID:
-        process.env.NEXT_PUBLIC_DOCS_FEEDBACK_SLACK_CHANNEL_ID,
-      SLACK_BOT_TOKEN: process.env.SLACK_BOT_TOKEN,
-      SLACK_FEEDBACK_API_BASE_URL: process.env.SLACK_FEEDBACK_API_BASE_URL,
-    },
+    server: {},
+    client: {},
+    runtimeEnv: {},
   });
