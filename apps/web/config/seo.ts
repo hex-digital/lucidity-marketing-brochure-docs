5751ef2a92d447d9ddd2c80e20e4d2416e0f1806
Jamie Warburton <jamie@hexdigital.com>
2026-04-29 13:45:20 +0100

    feat: shorten homepage SEO title

--- apps/web/config/seo.ts
+++ apps/web/config/seo.ts
@@ -4,7 +4,7 @@ export const seoConfig = {
   noIndex: env.NEXT_PUBLIC_NO_INDEX === 'true',
 
   title: 'Lucidity.js, the Enterprise Starter Kit for Sanity CMS',
-  homepageTitle: 'Lucidity.js by Hex Digital – The best way to start your Sanity project',
+  homepageTitle: 'Lucidity.js – The best way to start your Sanity project',
   description:
     'Cut 6 months off the delivery of your next Sanity project by building on top of Lucidity.js',
 };
